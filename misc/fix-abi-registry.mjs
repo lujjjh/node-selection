1e7b4dcf44e806f574238ad3eb6866b5c57a7dba
Jiahao Lu <lujjjh@gmail.com>
2022-04-03 16:29:19 +0800

    Update ABI registry

--- misc/fix-abi-registry.mjs
+++ misc/fix-abi-registry.mjs
@@ -1,7 +1,19 @@
 // https://github.com/prebuild/prebuild/issues/284
 
+import { spawnSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
+console.error('updating abi registry...');
+{
+  const cwd = dirname(fileURLToPath(await import.meta.resolve('node-abi/package.json')));
+  spawnSync('npm', ['i'], { cwd, shell: true }).output.toString('utf8');
+  spawnSync(process.argv[0], [fileURLToPath(await import.meta.resolve('node-abi/scripts/update-abi-registry.js'))], {
+    cwd,
+    shell: true,
+  });
+}
 
 const pathToAbiRegistry = fileURLToPath(await import.meta.resolve('node-abi/abi_registry.json'));
 
